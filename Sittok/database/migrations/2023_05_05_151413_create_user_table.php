904904d733f68acdf5b5f5269e6ef982ae821b2f
riafara <riafaradhila30@gmail.com>
2023-05-05 22:25:52 +0700

    migration user

---
+++ Sittok/database/migrations/2023_05_05_151413_create_user_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('user', function (Blueprint $table) {
+            $table->id('id');
+            $table->string('user_email');
+            $table->string('user_password');
+            $table->string('user_fullname');
+            $table->tinyInteger('level');
+            $table->string('alamat')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('user');
+    }
+};
